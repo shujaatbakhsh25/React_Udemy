7fa88b586538739c3d90417478a059f4d2828600
Shujaat Bakhsh <shujaatbakhsh25@gmail.com>
2019-07-16 14:22:57 -0400

    Components part 2

---
+++ seasons/src/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { lat: null };
+  }
+  render() {
+    window.navigator.geolocation.getCurrentPosition(
+      position => {
+        this.setState({ lat: position.coords.latitude });
+      },
+      err => console.log(err)
+    );
+    return <div>Latitutde : {this.state.lat}</div>;
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));

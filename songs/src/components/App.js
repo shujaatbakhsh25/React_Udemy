8b3e44ad7029306da8a7ad4f7fb372137314f6a8
Shujaat Bakhsh <shujaatbakhsh25@gmail.com>
2019-11-09 18:23:39 -0500

    added react-redux example

---
+++ songs/src/components/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { selectSong } from "./../actions";
+
+class App extends React.Component {
+  render() {
+    return <div>hello</div>;
+  }
+}
+
+export default App;

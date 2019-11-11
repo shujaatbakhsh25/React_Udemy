ec6b54e85e64b19ecd55e7f881d947d1c7528332
Shujaat Bakhsh <shujaatbakhsh25@gmail.com>
2019-11-10 20:44:55 -0500

    WIP

---
+++ blog/src/actions/index.js
@@ -0,0 +1,12 @@
+import jsonPlaceholder from "./../apis/jsonPlaceholder";
+import _ from "lodash";
+
+export const fetchPosts = () => async dispatch => {
+  const response = await jsonPlaceholder.get("/posts");
+  dispatch({ type: "FETCH_POSTS", payload: response.data });
+};
+export const fetchUser = id =>
+  _.memoize(async dispatch => {
+    const response = await jsonPlaceholder.get(`/users/${id}`);
+    dispatch({ type: "FETCH_USER", payload: response.data });
+  });
